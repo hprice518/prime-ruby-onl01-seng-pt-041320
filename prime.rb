73b944db618252d00b655a618d918fdca532df0c
Harrison Price <hprice518@gmail.com>
2020-04-26 22:24:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+# The prime numbers are the natural numbers greater than one that are not products of two smaller natural numbers.
+
+def prime?(num)
+  num <= 1 || num == 0 || num == 1 
+  return false 
+  
+  
+end
